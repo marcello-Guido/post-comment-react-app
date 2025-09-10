6749ff0b268d777efdd9488c11178425541cfd14
unknown <mguido2024@gmail.com>
2025-09-10 10:35:19 -0300

    Selectors

--- cypress/e2e/main.spec.cy.js
+++ cypress/e2e/main.spec.cy.js
@@ -1,9 +1,16 @@
 describe('template spec', () => {
+
+  const selectors = {
+    publishButton: '[data-qa="publish-button"]',
+
+  }
+
+
   it('passes', () => {
     cy.visit('localhost:5173/')
     cy.get('[data-qa="profile-box"]')
     cy.get('[data-qa="comment-input"]').first().type('Test 123')
-    cy.get('[data-qa="publish-button"]').first().click()
+    cy.get(selectors.publishButton).first().click()
     cy.get('[data-qa="comment-text"]').contains('Test 123')
     cy.get('[data-qa="comment-text"]').each(($element) => {
       if ($element.text() === 'Test 123') {
