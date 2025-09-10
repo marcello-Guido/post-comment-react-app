6af028c0c95c894db678af201efcfd12f32fa93c
unknown <mguido2024@gmail.com>
2025-09-10 10:45:23 -0300

    Selector update 2

--- cypress/e2e/main.spec.cy.js
+++ cypress/e2e/main.spec.cy.js
@@ -12,7 +12,7 @@ describe('template spec', () => {
     cy.get('[data-qa="comment-input"]').first().type('Test 123')
     cy.get(selectors.publishButton).first().click()
     cy.get(selectors.commentBox).contains('Test 123')
-    cy.get('[data-qa="comment-text"]').each(($element) => {
+    cy.get(selectors.commentBox).each(($element) => {
       if ($element.text() === 'Test 123') {
         cy.get($element).siblings('header').children('[data-qa="delete-button"]').click()
       }
